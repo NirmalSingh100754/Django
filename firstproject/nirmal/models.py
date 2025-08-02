b83b9b80fa2e3e451877b53425b0446521724b49
Nirmal Singh <nirmalsingh100754@hotmail.com>
2025-08-03 04:07:15 +0530

    Learned to configure database in django and store data like text, date,  images in it

--- firstproject/nirmal/models.py
+++ firstproject/nirmal/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPE_CHIOCE = [
+        ("MC", "Masala Chai"),
+        ("GC", "Ginger Chai"),
+        ("TC", "Tulsi Chai"),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="chais/")
+    date = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHIOCE)
+
+
+    def __str__(self):
+        return self.name
